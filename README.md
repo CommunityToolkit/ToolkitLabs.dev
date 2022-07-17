e2b553a53dc463184161dc3b081664f4b8a3a0ea
michael-hawker <24302614+michael-hawker@users.noreply.github.com>
2022-07-17 14:34:57 -0700

    Initial Test of GitHub Pages WASM site

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# ToolkitLabs.Dev
-Repository for https://toolkitlabs.dev/ site
+
+# ToolkitLabs.dev
+
+Repository for [Toolkit Labs](https://toolkitlabs.dev/) site.
+
+This site hosts the GitHub page generated from the [main repository here](https://github.com/CommunityToolkit/Labs-Windows) by [Uno Platform](https://platform.uno) tooling for WebAssembly.
---
+++ service-worker.js
@@ -0,0 +1,41 @@
+console.debug("[ServiceWorker] Initializing");
+
+let config = {};
+
+self.addEventListener('install', function (e) {
+    console.debug('[ServiceWorker] Installing offline worker');
+    e.waitUntil(
+        fetch("./package_e3557dfeb248e5b56fd478348ed80872f1eb78b3/uno-config.js")
+            .then(r => r.text()
+                .then(configStr => {
+                    eval(configStr);
+                    caches.open('package_e3557dfeb248e5b56fd478348ed80872f1eb78b3').then(function (cache) {
+                        console.debug('[ServiceWorker] Caching app binaries and content');
+                        return cache.addAll(config.offline_files);
+                    });
+                }
+                )
+            )
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(async function () {
+        try {
+            // Network first mode to get fresh content every time, then fallback to
+            // cache content if needed.
+            return await fetch(event.request);
+        } catch (err) {
+            return caches.match(event.request).then(response => {
+                return response || fetch(event.request);
+            });
+        }
+    }());
+});
+
+
+// managed

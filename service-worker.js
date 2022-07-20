6389862f775ebe11072578af57d357f84bb3413f
michael-hawker <24302614+michael-hawker@users.noreply.github.com>
2022-07-20 10:51:05 -0700

    Update site for launch!

--- service-worker.js
+++ service-worker.js
@@ -5,11 +5,11 @@ let config = {};
 self.addEventListener('install', function (e) {
     console.debug('[ServiceWorker] Installing offline worker');
     e.waitUntil(
-        fetch("./package_e3557dfeb248e5b56fd478348ed80872f1eb78b3/uno-config.js")
+        fetch("./package_3132fd751012733e01bf08de9a1ceeeed14acba5/uno-config.js")
             .then(r => r.text()
                 .then(configStr => {
                     eval(configStr);
-                    caches.open('package_e3557dfeb248e5b56fd478348ed80872f1eb78b3').then(function (cache) {
+                    caches.open('package_3132fd751012733e01bf08de9a1ceeeed14acba5').then(function (cache) {
                         console.debug('[ServiceWorker] Caching app binaries and content');
                         return cache.addAll(config.offline_files);
                     });

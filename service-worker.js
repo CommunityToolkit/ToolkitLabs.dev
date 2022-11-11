a23868172067de622f836be1692465745ab34c07
michael-hawker <24302614+michael-hawker@users.noreply.github.com>
2022-11-10 18:19:36 -0800

    November 10th Update for .NET Conf with Settings Controls and Rive Player updates, as well as Markdown Support
    Note: Debug build, having issues with Release builds...

--- service-worker.js
+++ service-worker.js
@@ -5,11 +5,11 @@ let config = {};
 self.addEventListener('install', function (e) {
     console.debug('[ServiceWorker] Installing offline worker');
     e.waitUntil(
-        fetch("./package_3132fd751012733e01bf08de9a1ceeeed14acba5/uno-config.js")
+        fetch("./package_1c377a79a7aea9a0dc299fdd09c7b29681f9039c/uno-config.js")
             .then(r => r.text()
                 .then(configStr => {
                     eval(configStr);
-                    caches.open('package_3132fd751012733e01bf08de9a1ceeeed14acba5').then(function (cache) {
+                    caches.open('package_1c377a79a7aea9a0dc299fdd09c7b29681f9039c').then(function (cache) {
                         console.debug('[ServiceWorker] Caching app binaries and content');
                         return cache.addAll(config.offline_files);
                     });
